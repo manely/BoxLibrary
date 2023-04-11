45bc0aeab12a277bf352df6f8a17d9b7909a0084
Mani Hamedani <mani.hamedani@icloud.com>
2023-04-11 18:21:00 +0330

    Box conforms Equatable and mix(with:) is implemented and tested.

--- Sources/BoxLibrary/Box.swift
+++ Sources/BoxLibrary/Box.swift
@@ -7,6 +7,17 @@
 
 import Foundation
 
-struct Box {
+struct Box: Equatable {
     var value: UInt
+    
+    func mix(with other: Box) -> Box? {
+        guard self.value == other.value else {
+            return nil
+        }
+        return Box(value: self.value + other.value)
+    }
+    
+    static func ==(_ lhs: Box, rhs: Box) -> Bool {
+        return lhs.value == rhs.value
+    }
 }
--- Tests/BoxLibraryTests/BoxTests.swift
+++ Tests/BoxLibraryTests/BoxTests.swift
@@ -13,4 +13,18 @@ final class BoxTests: XCTestCase {
         let b = Box(value: 2)
         XCTAssertEqual(2, b.value)
     }
+    
+    func testBoxMix() {
+        let b1 = Box(value: 2)
+        let b2 = Box(value: 2)
+        let mixed = b1.mix(with: b2)
+        XCTAssertEqual(Box(value: 4), mixed)
+    }
+    
+    func testBoxMixFail() {
+        let b1 = Box(value: 2)
+        let b2 = Box(value: 4)
+        let mixed = b1.mix(with: b2)
+        XCTAssertNil(mixed)
+    }
 }
